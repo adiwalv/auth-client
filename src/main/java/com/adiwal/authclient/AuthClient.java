acdb491e3769963f738ed8641f2b8e3cff110857
Vikas Adiwal <vikas.adiwal@qio.io>
2020-09-19 16:23:46 +0530

    added licens

--- src/main/java/com/adiwal/authclient/AuthClient.java
+++ src/main/java/com/adiwal/authclient/AuthClient.java
@@ -1,3 +1,7 @@
+/**
+ * © Vikas Adiwal (adiwalv@gmail.com) 2020. All rights reserved.
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION OF VIKAS ADIWAL.
+ */
 package com.adiwal.authclient;
 
 import org.springframework.beans.factory.annotation.Value;
